b44973d574043a9d6a9b4e488dc11b20978559a2
niba291 <nibachavez291@gmail.com>
2024-11-16 18:01:36 -0300

    V2.2

---
+++ src/config/env.ts
@@ -0,0 +1,102 @@
+export const experience        = [
+    {
+        title           : "DESARROLLADOR FULL STACK | 2024 JULIO - 2024 NOVIEMBRE",
+        description     : [
+            "• Implementación de microservicios utilizando Docker y metodologia SOLID, conectados a un controlador en Symfony para mejorar la modularidad y escalabilidad del sistema.",
+            "• Desarrollo de un bot en Node.js para automatizar diversas tareas dentro de la plataforma.",
+            "• Integración de la API de Jira para gestionar organizaciones, asignar personas a tareas y generar tickets de soporte, optimizando el flujo de trabajo interno.",
+            "•  Diseño y mantenimiento de un módulo para el sistema de facturación, implementando mejoras significativas en el rendimiento de consultas y optimizando la performance general de la aplicación.",
+        ],
+        company         : "POLITE SPA",
+        tags            : ["NODE.JS", "REACT-NATIVE", "PHP", "SYMFONY"]
+    },
+    {
+        title           : "DESARROLLADOR FULL STACK | 2024 FEBRERO - 2024 MAYO",
+        description     : [
+            "• Implementación de microservicios utilizando Docker y metodologia SOLID, conectados a un controlador en Symfony para mejorar la modularidad y escalabilidad del sistema.",
+            "• Creación de una API para la gestión de Excel, modularizando su uso en la facturación y monitoreando errores mediante la integración de Sentry.",
+            "• Modificación y adaptación de archivos XML para el envío de facturas al SII.",
+            "• Estructuración de la base de datos para soportar las implementaciones desarrolladas y optimizar el sistema de facturación.",
+        ],
+        company         : "WEBFACTURA SPA",
+        tags            : ["PHP", "SYMFONY", "TWIG"]
+    },
+    {
+        title           : "DESARROLLADOR FULL STACK | 2023 MARZO - 2024 FEBRERO",
+        description     : [
+            "• Desarrollo de API de facturación electrónica con firma de XML utilizando PFX y envío automático al SII, además de la integración con una casilla de correo para procesar documentos.",
+            "• Desarrollo de una aplicación de punto de venta full-stack con interfaz dinámica controlada por JSON, implementada con React en el front-end y microservicios en Python y Node.js para la gestión de facturación e inventario, usando MongoDB y MySQL.",
+            "• Creación de una API para Google Sheets, asegurando la sincronización constante de las hojas de cálculo, facilitando la integración para casi la mitad de los clientes.",
+            "• Apoyo en la organización y liderazgo del equipo, facilitando la incorporación de nuevas tecnologías y asumiendo responsabilidades en su implementación y puesta en marcha."
+        ],
+        company         : "ACHL SPA",
+        tags            : ["PYTHON", "NODE.JS", "REACT", "REACT-NATIVE", "PHP", "LARAVEL", "TAILWINDSCSS"]
+    },
+    {
+        title           : "DESARROLLADOR FULL STACK | 2022 MAYO - 2023 MARZO",
+        description     : [
+            "• Reestructuración de una aplicación GPS desarrollada en CodeIgniter, adaptándola para usuarios individuales en lugar de flotas, con funcionalidades adicionales para distintos clientes.",
+            "• Configuración de Mikrotik y desarrollo de módulos para gestionar redes, incluyendo la creación de VLANs, máscaras de red, entre otros.",
+            "• Desarrollo de una API para WhatsApp utilizando un móvil con Termux, conectando una aplicación en Java con un programa en Python mediante sockets para automatizar el envío de mensajes.",
+            "• Implementación de notificaciones push con Firebase para alertar a los usuarios cuando un vehículo salía de una geocerca, activando un corta corriente o avisando al usuario según el caso.",
+        ],
+        company         : "TIC NETWORKS AND SECURITY LTDA",
+        tags            : ["CODEIGNITER", "PHP", "PYTHON", "JAVA", "ANTROID", "POWER BI", "BOOTSTRAP"]
+    },
+    {
+        title           : "DESARROLLADOR FULL STACK | 2021 AGOSTO - 2022 MARZO",
+        description     : [
+            "• Realización de scraping con C# utilizando Selenium para extraer boletas electrónicas desde una API con autenticación JWT.",
+            "• Mantenimiento del programa administrativo de la empresa, incluyendo la creación de un módulo para notas de venta y consultas de boletas de honorarios.",
+            "• Mantenimiento y desarrollo de nuevas características para boletas y facturas electrónicas, asegurando su correcta implementación en el sistema.",
+            "• Diseño y desarrollo de una alerta estándar para la plataforma, mejorando la experiencia del usuario.",
+            "• Codificación de XML para la integración y envío de documentos electrónicos, incluyendo la mitigación de errores al convertir archivos a UTF-8 BOM.",
+        ],
+        company         : "CONTALINE S.A",
+        tags            : ["ASP", "BOOTSTRAP", "C#", "SCRAPING SELENIUM", "VISUAL BASIC", ".NET"]
+    }
+];
+
+export const proyect           = [
+    {
+        title           : "PDF-EDITOR",
+        description     : "Sitio web para crear pdf basico en tiempo real donde podrás descargar el archivo pdf, creado con React, React-pdf y TailwindCSS",
+        github          : "https://github.com/niba291/PDF-EDITOR",
+        live            : "https://niba291.github.io/PDF-EDITOR/",
+        img             : "https://github.com/niba291/PDF-EDITOR/assets/63934510/b60f1d84-098a-439c-a55f-e2016965e0ab",
+        tags            : ["REACT", "TAILWINDSCSS", "REACT-PDF"]
+    },
+    {
+        title           : "POKEAPI",
+        description     : "Sitio web sencillo diseñado con TailwindCSS y React. Utiliza una API externa llamada PokeAPI para mostrar todos los Pokémon, buscar Pokémon y obtener sus estadísticas y tipos.",
+        github          : "https://github.com/niba291/POKEAPI",
+        live            : "https://niba291.github.io/POKEAPI/",
+        img             : "https://github.com/niba291/POKEAPI/assets/63934510/2a789344-66a1-45af-bfa6-85e3e48eb764",
+        tags            : ["REACT", "TAILWINDSCSS"]
+    },
+    {
+        title           : "To-do",
+        description     : "Sitio web para realizar to-do list",
+        github          : "https://github.com/niba291/To-do",
+        live            : "https://niba291.github.io/To-do/",
+        img             : "https://raw.githubusercontent.com/niba291/To-do/refs/heads/master/demo_1.png",
+        tags            : ["REACT", "TAILWINDSCSS"]
+    }
+];
+
+export const certificate        = [
+    {
+        title           : "React Native CLI: Aplicaciones nativas para IOS y Android",
+        description     : "Curso de react native donde se aprende la base para desarrollo moviles.",
+        url             : "https://www.udemy.com/certificate/UC-4152edf7-bdc6-400c-9375-59bd5774bad0/",
+        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-4152edf7-bdc6-400c-9375-59bd5774bad0.jpg?v=1725413337000",
+        tags            : ["REACT NATIVE", "TYPESCRIPT", "MOVIL", "REACT"]
+    },
+    {
+        title           : "NodeJS: De cero a experto",
+        description     : "Curso de Node de cero a experto, realizando Api con expres.",
+        url             : "https://www.udemy.com/certificate/UC-b67d65bb-96af-4cd9-903f-c5f988533344/",
+        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-b67d65bb-96af-4cd9-903f-c5f988533344.jpg?v=1727795491000",
+        tags            : ["NODE JS", "EXPRESS", "API", "BACK-END"]
+    }
+];
