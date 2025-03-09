4ff4d1aa2b3666e8eb751e77195b3d35844486d3
niba291 <nibachavez291@gmail.com>
2025-03-09 12:46:32 -0300

    fix: update certificate

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 import imageLandingPage     from "../images/landingPage.webp";
 
-export const experience        = [
+export const experience = [
     {
         title           : "DESARROLLADOR FULL STACK | 2024 JULIO - ACTUALMENTE",
         description     : [
@@ -59,7 +59,7 @@ export const experience        = [
     }
 ];
 
-export const proyect           = [
+export const proyect    = [
     {
         title           : "DIGITALDEVSOLUTIONS",
         description     : "Landing page creada con Astro y Tailwind CSS, enfocada en promover servicios de programación web. Diseño rápido y responsivo. Optimizada para una experiencia fluida y conversión de clientes.",
@@ -97,10 +97,10 @@ export const proyect           = [
 export const certificate        = [
     {
         title           : "React Native CLI: Aplicaciones nativas para IOS y Android",
-        description     : "Curso de Next.js para desarrolladores de React, TypeScript, APIs, y autenticación personalizada hasta despliegue y optimización de aplicaciones.",
-        url             : "https://www.udemy.com/certificate/UC-137830a0-1dfb-4505-aad9-eed73fde5274/",
-        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-137830a0-1dfb-4505-aad9-eed73fde5274.jpg?v=1737460551000",
-        tags            : ["REACT", "TYPESCRIPT", "NEXT.JS", "API", "BACK-END"]
+        description     : "Curso de react native donde se aprende la base para desarrollo moviles.",
+        url             : "https://www.udemy.com/certificate/UC-4152edf7-bdc6-400c-9375-59bd5774bad0/",
+        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-4152edf7-bdc6-400c-9375-59bd5774bad0.jpg?v=1725413337000",
+        tags            : ["REACT NATIVE", "TYPESCRIPT", "MOVIL", "REACT"]
     },
     {
         title           : "NodeJS: De cero a experto",
@@ -111,9 +111,9 @@ export const certificate        = [
     },
     {
         title           : "Next.js: El framework de React para producción",
-        description     : "Curso de Node de cero a experto, realizando Api con expres.",
-        url             : "https://www.udemy.com/certificate/UC-b67d65bb-96af-4cd9-903f-c5f988533344/",
-        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-b67d65bb-96af-4cd9-903f-c5f988533344.jpg?v=1727795491000",
-        tags            : ["NODE JS", "EXPRESS", "API", "BACK-END"]
+        description     : "Curso de Next.js para desarrolladores de React, TypeScript, APIs, y autenticación personalizada hasta despliegue y optimización de aplicaciones.",
+        url             : "https://www.udemy.com/certificate/UC-137830a0-1dfb-4505-aad9-eed73fde5274/",
+        img             : "https://udemy-certificate.s3.amazonaws.com/image/UC-137830a0-1dfb-4505-aad9-eed73fde5274.jpg?v=1737460551000",
+        tags            : ["NODE JS", "EXPRESS", "API", "BACK-END", "NEXT JS"]
     }
 ];
